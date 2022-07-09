97214b21cb77a9fac3b62cc1aa476add093ac26e
Guilherme Aguiar <kadeguilherm@gmail.com>
2022-07-09 15:07:21 -0300

    Colocando avatares

---
+++ public/index.js
@@ -0,0 +1,6 @@
+const image = document.createElement('img')
+
+image.src = 'https://raw.githubusercontent.com/kadeguilherme/api-pokemon/460e2ec1ae2400e37bc137aada2cb6779bf4da2c/public/emoji.svg'
+
+document.querySelector('.images').appendChild(image)
+console.log("oi");
